9d5d6f98e009862b860123dda5e63d7940d9d7ff
Cooper Nissenbaum <cnissenbaum@stuy.edu>
2019-01-08 15:14:40 -0500

    Started Game

--- BlackJackGame.c
+++ BlackJackGame.c
@@ -18,6 +18,7 @@ void Print_Deck() {
   int i = 0;
   while (i<52){
     Print_Card(Deck[i]);
+    i++;
   }
 }
 
@@ -25,24 +26,41 @@ void Initialize_Deck() {
   int i = 0;
   while (i < 52) {
     Deck[i] = i;
+    i++;
   }
 }
 
 
 void Shuffle_Deck() {
   printf("Shuffling...\n");
-  time = NULL;
-  srand();
+  srand(time(NULL));
+  int i = 0;
+  while (i < 1000) {
+    int rand0 = random();
+    rand0 = rand0%52;
 
-  int rand0 = random();
-  rand0 = rand0%52;
+    int rand1 = random();
+    rand1 = rand1%52;
 
-  int rand1 = random();
-  rand1 = rand1%52;
+    int help = Deck[rand0];
+    Deck[rand0] = Deck[rand1];
+    Deck[rand1] = help;
 
-  int help = Deck[rand0];
-  Deck[rand0] = Deck[rand1];
-  Deck[rand1] = help;
+    i++;
+  }
+}
 
+int main(int argc, char const *argv[]) {
+  Initialize_Deck();
+  printf("Begin\n");
   Print_Deck();
+  printf("------------------------\n");
+  Shuffle_Deck();
+  printf("------------------------\n");
+  Print_Deck();
+  printf("------------------------\n");
+  Initialize_Deck();
+  Print_Deck();
+
+  return 0;
 }
