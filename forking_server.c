77f5335c151acecac7988aaa42bc5a7f05e29c47
horazio <hfusco@stuy.edu>
2019-01-12 16:39:31 -0500

    started integrating the game into the server

--- forking_server.c
+++ forking_server.c
@@ -3,22 +3,26 @@
 void process(char *s);
 void subserver(int from_client);
 char * ports[] = {"9002", "9003", "9004", "9005"};
-//void listen()
+
+char * deck = Initialize_Deck();
 
 int main() {
   
+  int num_players = MAX_PLAYERS;
   int eternal_socket;
   int listen_socket;
   int client_socket;
   int f;
-  struct player playas[4];
+  
+  
+  struct player playas[num_players];
   int i = 0;
   char buffer[BUFFER_SIZE];
   
   
   eternal_socket = server_setup("9001");  
   printf("Waiting for four players\n");
-  while(i < 4){
+  while(i < num_players){
     
     client_socket = server_connect(eternal_socket);
     write(client_socket, ports[i], 5);
@@ -33,11 +37,21 @@ int main() {
     i++;
   }
   
+  
+  
+  for(i = 0; i < num_players; i++){
+    
+    
+    
+  }
+  
+  
+  /*
   printf("Something %s, %d\n", playas[0].name, playas[0].clySock);
   printf("Something %s, %d\n", playas[1].name, playas[1].clySock);
   printf("Something %s, %d\n", playas[2].name, playas[2].clySock);
   printf("Something %s, %d\n", playas[3].name, playas[3].clySock);
-  
+  */
    
       
       
